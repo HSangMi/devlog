4069ffc8104619f1197b485eaa6ae688c45b5999
han32 <han32@inzent.com>
2024-05-10 17:35:20 +0900

    feat : 썸네일 컨네이너 정렬기능 추가, css 변경

--- src/components/thumbnail-container/index.jsx
+++ src/components/thumbnail-container/index.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 import './index.scss'
 
-export const ThumbnailContainer = React.memo(({ children }) => (
-  <div className="thumbnail-container">{children}</div>
-))
+export const ThumbnailContainer = React.memo(({ children }) => {
+  // children을 복사하여 배열로 만들기
+  const childrenArray = React.Children.toArray(children);
+
+  // 날짜 속성을 기준으로 정렬
+  childrenArray.sort((a, b) => {
+    // 날짜 속성을 비교하여 정렬
+    const dateA = new Date(a.props.node.frontmatter.date);
+    const dateB = new Date(a.props.node.frontmatter.date);
+    return dateA - dateB;
+  });
+  return <div className="thumbnail-container">{childrenArray}</div>;
+});
