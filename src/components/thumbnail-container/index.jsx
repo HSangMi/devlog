66b8ec29449e251c371e1f466c4b40514441581f
han32 <han32@inzent.com>
2024-05-14 10:46:18 +0900

    fix: 썸네일 컨테이너 정렬

--- src/components/thumbnail-container/index.jsx
+++ src/components/thumbnail-container/index.jsx
@@ -5,13 +5,13 @@ import './index.scss'
 export const ThumbnailContainer = React.memo(({ children }) => {
   // children을 복사하여 배열로 만들기
   const childrenArray = React.Children.toArray(children);
-
   // 날짜 속성을 기준으로 정렬
   childrenArray.sort((a, b) => {
     // 날짜 속성을 비교하여 정렬
     const dateA = new Date(a.props.node.frontmatter.date);
-    const dateB = new Date(a.props.node.frontmatter.date);
-    return dateA - dateB;
+    const dateB = new Date(b.props.node.frontmatter.date);
+    // return dateA - dateB;  // 오름차순
+    return dateB - dateA;
   });
   return <div className="thumbnail-container">{childrenArray}</div>;
 });
